6d2f43283ce9761e5c183047147d230a5457ef04
robbiabd <robbyabd10@gmail.com>
2020-08-23 09:36:21 +0700

    profile image

--- src/container/layout/Profile/index.js
+++ src/container/layout/Profile/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "../layout.css";
 import { EditOutlined } from "@ant-design/icons";
-import { Layout, Card, Avatar } from "antd";
+import { Layout, Card, Avatar, Badge, Row, Col } from "antd";
 const { Content } = Layout;
+
+const bannerProfile = {
+  width: "100%",
+  backgroundImage:
+    "url(https://images.pexels.com/photos/268966/pexels-photo-268966.jpeg?cs=srgb&dl=pexels-pixabay-268966.jpg&fm=jpg)",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  position: "relative",
+  height: "190px",
+};
 
 const Profile = () => {
   return (
@@ -13,10 +24,7 @@ const Profile = () => {
           style={{ padding: 24, minHeight: 360 }}
         >
           <div className="site-card-border-less-wrapper">
-            <Card
-              bordered={false}
-              style={{ width: "100%", backgroundColor: "#c4c4c4" }}
-            >
+            <Card bordered={false} style={bannerProfile}>
               <Avatar
                 style={{
                   color: "#f56a00",
@@ -25,6 +33,27 @@ const Profile = () => {
                 }}
                 icon={<EditOutlined />}
               />
+              <div style={{ position: "absolute", marginTop: "100px" }}>
+                <Row>
+                  <Col span={12}>
+                    <span className="avatar-item">
+                      <Badge
+                        count={<EditOutlined />}
+                        style={{
+                          color: "#f56a00",
+                          fontSize: "20px",
+                        }}
+                      >
+                        <Avatar
+                          size={120}
+                          shape="square"
+                          src="https://avatars1.githubusercontent.com/u/42019007?s=460&u=d12157817ce2c4795d2d040644a9077edf9acaa6&v=4"
+                        />
+                      </Badge>
+                    </span>
+                  </Col>
+                </Row>
+              </div>
             </Card>
           </div>
         </div>
